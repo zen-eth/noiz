8fbaf93eb15c0fadf99fea1e1d75145938e8c595
bing <eightfilms@keemail.me>
2024-11-30 14:10:42 +0800

    wip: hkdf, hmachash

--- src/root.zig
+++ src/root.zig
@@ -12,11 +12,33 @@ const Blake2b512 = std.crypto.hash.blake2.Blake2b512;
 
 const CipherState = @import("./cipher.zig").CipherState;
 
-const SymmetricState = struct {
-    cipher_state: CipherState,
-    ck: []u8,
-    h: []u8,
-};
+pub fn SymmetricState(comptime H: type, comptime C: type) type {
+    const Hash_ = Hash(H);
+
+    const HASHLEN = Hash_.len;
+
+    return struct {
+        cipher_state: CipherState,
+        ck: [HASHLEN]u8,
+        h: [HASHLEN]u8,
+
+        const Self = @This();
+
+        pub fn init(allocator: std.mem.Allocator, protocol_name: []const u8) Self {
+            const h = if (protocol_name.len <= HASHLEN) {} else {
+                Hash_.hash(protocol_name, .{});
+            };
+
+            const cipher_state = CipherState(C, allocator);
+            cipher_state.init([_]u8{0} ** 32);
+            return .{
+                .cipher_state = cipher_state,
+                .ck = h,
+                .h = h,
+            };
+        }
+    };
+}
 
 const HandshakeState = struct {
     const Self = @This();
@@ -30,33 +52,99 @@ const HandshakeState = struct {
 /// Only these hash functions are supported in accordance with the spec: `Sha256`, `Sha512`, `Blake2s256`, `Blake2b512`.
 ///
 /// https://noiseprotocol.org/noise.html#hash-functions
-// TODO: implement hmac-hash and hkdf
-fn Hash(comptime H: type) !type {
+fn Hash(comptime H: type) type {
     const _Hash = H;
 
     const HASHLEN = comptime switch (H) {
         Sha256, Blake2s256 => 32,
         Sha512, Blake2b512 => 64,
-        else => return error.UnsupportedHash,
+        else => @compileError(std.fmt.comptimePrint("Unsupported hash: {any}", .{H})),
     };
 
     const BLOCKLEN = comptime switch (H) {
         Sha256, Blake2s256 => 64,
         Sha512, Blake2b512 => 128,
-        else => return error.UnsupportedHash,
+        else => @compileError(std.fmt.comptimePrint("Unsupported hash: {any}", .{H})),
     };
 
     return struct {
+        const len = HASHLEN;
+
+        const Self = @This();
+
+        fn init() Self {
+            return .{};
+        }
+
         /// Hashes some arbitrary-length data with a collision-resistant cryptographic hash function.
         ///
         /// Returns an output of `HASHLEN` bytes.
-        fn hash(input: []const u8, options: anytype) [HASHLEN]u8 {
-            _ = BLOCKLEN;
+        fn hash(input: []const u8) [HASHLEN]u8 {
             var out: [HASHLEN]u8 = undefined;
-            _Hash.hash(input, &out, options);
+            _Hash.hash(input, &out, .{});
             return out;
         }
+
+        /// https://www.ietf.org/rfc/rfc2104.txt
+        fn hmacHash(allocator: std.mem.Allocator, key: []const u8, data: []const u8) ![]const u8 {
+            const B = BLOCKLEN;
+
+            const ipad = [_]u8{0x36} ** B;
+            const opad = [_]u8{0x5C} ** B;
+
+            // 1. append zeroes at the end of K to create a B byte string
+            var zeroes = try allocator.alloc(u8, B - key.len);
+            for (0..zeroes.len) |i| {
+                zeroes[i] = 0;
+            }
+            var appended = try std.mem.concat(allocator, u8, .{ key, zeroes[0..] });
+            // (2) XOR B byte string with ipad
+            for (0..appended.len) |i| {
+                appended[i] ^= 0x36;
+            }
+            const key_xor_ipad = key ^ ipad;
+            // (3) append 'text' to B byte string from (2)
+            const stream = try std.mem.concat(allocator, u8, .{ key_xor_ipad, data });
+
+            // (4) apply H to stream generated in (3)
+            const h = hash(stream);
+
+            // (5) XOR B byte string with opad
+            const key_xor_opad = key ^ opad;
+
+            // (6) append 'h' from (4) to B byte string from (5)
+            const hh = try std.mem.concat(allocator, u8, .{ key_xor_opad, h });
+
+            // (7) apply H to stream generated in (6) and output the result
+            const result = hash(hh);
+            return result;
+        }
+
+        fn HKDF(
+            allocator: std.mem.Allocator,
+            chaining_key: []const u8,
+            input_key_material: []const u8,
+            num_outputs: u8,
+        ) struct { []const u8, []const u8, ?[]const u8 } {
+            std.debug.assert(input_key_material.len == 0 or input_key_material.len == 32);
+
+            const temp_key = hmacHash(allocator, chaining_key, input_key_material);
+            const output1 = hmacHash(allocator, temp_key, &[_]u8{0x01});
+            const bytes = [_]u8{0x02};
+            const output2 = hmacHash(allocator, temp_key, std.mem.concat(allocator, u8, .{ output1, bytes }));
+            if (num_outputs == 2) return .{ output1, output2, null };
+            const output3 = hmacHash(allocator, temp_key, std.mem.concat(allocator, u8, .{ output2, [_]u8{0x03} }));
+
+            return .{ output1, output2, output3 };
+        }
     };
+}
+
+test "hash" {
+    const h = Hash(Sha256);
+    const ck = [_]u8{1} ** 32;
+    const ikm = [_]u8{};
+    _ = h.HKDF(std.testing.allocator, &ck, &ikm, 3);
 }
 
 /// A Noise Diffie-Hellman function.
