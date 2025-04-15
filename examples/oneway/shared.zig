8603ff5af87cd2c4289b80f0a9af6a3067b574c5
bing <spiralladder@keemail.me>
2025-04-15 11:03:18 +0800

    example(oneway): formatting + prologue

--- examples/oneway/shared.zig
+++ examples/oneway/shared.zig
@@ -1,8 +1,9 @@
 //! Contains a list of agreed upon constants prior to handshake.
+
 pub const PSK: []const u8 = "A complicated enough system eventually becomes ensouled.";
 pub const RESPONDER_SK_HEX = "52fbe3721d1adbe312d270ca2db5ce5bd39ddc206075f3a8f06d422619c8eb5d";
 pub const RESPONDER_PK_HEX = "435ce8a8415ccd44de5e207581ac7207b416683028bcaecc9eb38d944e6f900c";
 pub const PROTOCOL_NAME = "Noise_Xpsk1_25519_ChaChaPoly_BLAKE2s";
 pub const IP = "127.0.0.1";
 pub const PORT = 9999;
-pub const PROLOGUE = "";
+pub const PROLOGUE = "milady";
