8b779f5a7ae2f25da03364ae3057b8822c957ff8
guha-rahul <52607971+guha-rahul@users.noreply.github.com>
2025-04-20 22:14:24 +0530

    Update root.zig

--- src/root.zig
+++ src/root.zig
@@ -8,6 +8,9 @@ pub const DH = @import("dh.zig").DH;
 ///
 ///See: http://www.noiseprotocol.org/noise.html#message-format
 pub const MAX_MESSAGE_LEN = 65535;
+pub const cipher = @import("cipher.zig");
+pub const hash   = @import("hash.zig");
+pub const symmetric_state = @import("symmetric_state.zig");
 
 test {
     _ = @import("cipher.zig");
